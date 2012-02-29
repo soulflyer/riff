d8b689ed8dbfbd6db2c7fc1a64d988ce51403ba6
Iain <public@soulflyer.co.uk>
2012-02-29 07:48:04 +0700

    renamed play-riff play

--- src/riff/core.clj
+++ src/riff/core.clj
@@ -7,7 +7,7 @@
         env (env-gen (perc 0.1 0.2) :action FREE)]
     (* src env)))
 
-(def metro (metronome 12))
+(def metro (metronome 30))
 
 (defprotocol IRiff
   (riff-root [this] [this new-riff-root]
@@ -34,23 +34,24 @@
     (reset! riff-notes new-riff-notes))
   (riff-offsets [this] @riff-offsets)
   (riff-offsets [this new-riff-offsets]
-    (reset! offsets new-riff-offsets))
+    (reset! riff-offsets new-riff-offsets))
   (riff-length [this] @riff-length)
   (riff-length [this new-riff-length]
     (reset! riff-length new-riff-length)))
 
-(defn play-riff
-  [riff metro beat]
-  (let [notes-to-play (degrees->pitches (riff-notes riff) (riff-scale riff) (riff-root riff))]
-      (dorun
-       (map (fn [note offset]
-              (at (metro (+ beat offset))
-                  (beep note)
-                  (info "RiffPlayer note: " note
-                        "  time: " (metro (+ beat offset)))))
-            notes-to-play
-            (riff-offsets riff)))
-      (apply-at (metro (inc beat))  #'play-riff riff[metro (inc beat)])))
+(defn play
+  ([riff metro] (play riff metro (metro)))
+  ([riff metro beat]
+     (let [notes-to-play (degrees->pitches (riff-notes riff) (riff-scale riff) (riff-root riff))]
+       (dorun
+        (map (fn [note offset]
+               (at (metro (+ beat offset))
+                   (beep note)
+                   (info "RiffPlayer note: " note
+                         "  time: " (metro (+ beat offset)))))
+             notes-to-play
+             (riff-offsets riff)))
+       (apply-at (metro (inc beat))  #'play riff[metro (inc beat)]))))
 
 (defn riff [riff-root riff-scale riff-notes riff-offsets riff-length]
   (let [riff-root (atom riff-root)
@@ -62,4 +63,4 @@
 
 (def riffp (riff :c5 :major [:i :iii :v :i+] [0 1/4 1/2 3/4] 4))
 
-(play-riff riffp metro (metro))
+(play riffp metro)
