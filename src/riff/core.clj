1ff26d9809252debaa069cd40d11ea2d21bbb5b8
Iain <public@soulflyer.co.uk>
2012-03-08 11:44:51 +0700

    working correctly with new fns move-degrees and make-degree in overtone/music/pitch.clj (soulflyer/overtone metronome branch)

--- src/riff/core.clj
+++ src/riff/core.clj
@@ -7,7 +7,7 @@
         env (env-gen (perc 0.1 0.2) :action FREE)]
     (* src env)))
 
-(def metro (metronome 30))
+(def metro (metronome 130))
 
 (defprotocol IRiff
   (riff-root [this] [this new-riff-root]
@@ -15,13 +15,13 @@
   (riff-scale [this] [this new-riff-scale]
     "returns the current scale or sets it to new-scale")
   (riff-notes [this] [this new-riff-notes]
-    "Returns riff or sets it to new-riff")
+    "Returns notes or sets it to new-riff-notes")
   (riff-offsets [this] [this new-riff-offsets]
-    "Returns offsets or sets it to new-offsets")
-  (riff-length [this] [this new-riff-length]
-    "returns length or sets it to new-riff-length"))
+    "Returns offsets or sets it to new-riff-offsets")
+  (riff-offset [this] [this new-riff-offset]
+    "Returns offset or sets it to new-riff-offset"))
 
-(deftype Riff [riff-root riff-scale riff-notes riff-offsets riff-length]
+(deftype Riff [riff-root riff-scale riff-notes riff-offsets riff-offset]
   IRiff
   (riff-root [this] @riff-root)
   (riff-root [this new-riff-root]
@@ -35,9 +35,19 @@
   (riff-offsets [this] @riff-offsets)
   (riff-offsets [this new-riff-offsets]
     (reset! riff-offsets new-riff-offsets))
-  (riff-length [this] @riff-length)
-  (riff-length [this new-riff-length]
-    (reset! riff-length new-riff-length)))
+  (riff-offset [this] @riff-offset)
+  (riff-offset [this new-riff-offset]
+    (reset! riff-offset new-riff-offset)))
+
+(defn riff [riff-root riff-scale riff-notes riff-offsets]
+  (let [riff-root (atom riff-root)
+        riff-scale (atom riff-scale)
+        riff-notes (atom riff-notes)
+        riff-offsets (atom riff-offsets)
+        riff-offset (atom 0)]
+    (Riff. riff-root riff-scale riff-notes riff-offsets riff-offset)))
+
+(def riffp (riff :c5 :major [:i :iii :v :i+] [0 1.5 2 3]))
 
 (defn play
   ([riff metro] (play riff metro (bar metro)))
@@ -55,15 +65,5 @@
              notes-to-play
              (riff-offsets riff)))
        (apply-at (bar metro (inc br))  #'play riff [metro (inc br)]))))
-
-(defn riff [riff-root riff-scale riff-notes riff-offsets riff-length]
-  (let [riff-root (atom riff-root)
-        riff-scale (atom riff-scale)
-        riff-notes (atom riff-notes)
-        riff-offsets (atom riff-offsets)
-        riff-length (atom riff-length)]
-    (Riff. riff-root riff-scale riff-notes riff-offsets riff-length)))
-
-(def riffp (riff :c5 :major [:i :iii :v :i+] [0 1 2 3] 4))
 
 (play riffp metro)
